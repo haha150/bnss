2da08e8e3bbf33d0259e853ed610b4c105dba668
Ali <symeri@kth.se>
2018-02-28 11:43:50 +0100

    lib

--- src/main/java/org/bnss/controller/WebController.java
+++ src/main/java/org/bnss/controller/WebController.java
@@ -18,38 +18,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebController {
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ResponseEntity<String> welcome() {
-		System.out.println("post login");
-		return new ResponseEntity<>("Hello world", HttpStatus.OK);
-    }
 	
-	@PreAuthorize("hasAuthority('ROLE_USER')")
-	@RequestMapping(value = "/user")
-    public String user(Model model, Principal principal) {
-         
-        UserDetails currentUser 
-          = (UserDetails) ((Authentication) principal).getPrincipal();
-        model.addAttribute("username", currentUser.getUsername());
-        return "user";
-    }
-	
-	@GetMapping("/test")
-	public String home(Principal principal){
-		return String.format("Hello %s!", principal.getName());
-	}
-	
-	@RequestMapping(value = "/logged", method = RequestMethod.GET)
-    public String helloController()
-    {
-        String loggedUser;
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        if (principal instanceof UserDetails) {
-            loggedUser = ((UserDetails) principal).getUsername();
-        } else {
-            loggedUser = principal.toString();
-        }
-        return "X.509 authentication done! REST client has been identified as ["+loggedUser+"] ";
-    }
 }
